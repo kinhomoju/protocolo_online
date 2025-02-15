5ccb30dd3849f07a587ff1302cf5948504db7e83
kinhomoju <walkerson.cursos@gmail.com>
2025-02-14 21:24:26 -0300

    Adiciona python-decouple e configurações de ambiente

--- protocolo_online/settings.py
+++ protocolo_online/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-# filepath: /workspaces/protocolo_online/protocolo_online/settings.py
 from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,13 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qx-3y)+@%!8ow^5iid!r!y+84^4+&q=hu#z05zi5(70vwvif+r'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Application definition
